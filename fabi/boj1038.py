cf7d124836308ac5a03e182fd06fdbce3d52052c
fabichoi <fabichoi@fabichoi.local>
2020-04-18 17:41:39 +0900

    solved boj 1038

--- fabi/boj1038.py
+++ fabi/boj1038.py
@@ -1,1 +1,17 @@
-print('boj 1038')
+from itertools import combinations
+n = int(input())
+a = list(range(0,10))
+res = []
+for i in range(1, 10):
+    combi = list(combinations(a, i))
+    for c in combi:
+        c = list(c)
+        c.sort(reverse=True)
+        res.append(''.join(map(str,c)))
+res.append('9876543210')
+res = list(map(int, res))
+res.sort()
+if n > 1022:
+    print(-1)
+else:
+    print(res[n])
