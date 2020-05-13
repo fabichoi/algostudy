3483c0527e87c320c504507d477afb7f9eebcf7e
fabichoi <fabichoi@fabichoi.local>
2020-05-13 22:31:23 +0900

    pro_dp_snitch

--- fabi/pro_dp_snitch.py
+++ fabi/pro_dp_snitch.py
@@ -1,6 +1,16 @@
 def solution(money):
-    answer = 0
-    return answer
+    m = len(money)
+    if m <= 3:
+        return max(money[0], money[1], money[2])
+
+    def dp(n):
+        if n < 0:
+            return 0
+        if n < 3:
+            return money[n]
+        return max(money[n] + dp(n-2), money[n-1] + dp(n-3))
+
+    return dp(m-1)
 
 if __name__ == '__main__':
-    print(solution([1,2,3,1]))
+    print(solution([5,9,7,29]))
