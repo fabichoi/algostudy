158646ce59ff894cfe4616064e7b261224f65065
fabichoi <fabichoi@fabichoi.local>
2020-05-20 22:56:54 +0900

    5th_week source upload

--- fabi/5th_week/boj1660.py
+++ fabi/5th_week/boj1660.py
@@ -1,43 +1,12 @@
-'''
-balls = [i for i in range(300001)]
-tri = [0]
-tetra = [0]
-n = 15
-
-# 각 사면체당 대포알 갯수 구하기
-for i in range(1, 122):
-    tri.append(tri[-1] + i)
-    tetra.append(tri[-1] + tetra[-1])
-
-# n이 300000 까지 구하기
-li = [0 for _ in range(200)]
-d = [0 for _ in range(300001)]
-for i in range(1,200):
+li = [0 for _ in range(122)]
+d = [i for i in range(300001)]
+for i in range(1,122):
     li[i] = i * (i+1) * (i+2) // 6
 n = int(input())
-for i in range(1, n+1):
+for i in range(2, n+1):
     if li[i] > n:
         break
     for j in range(li[i], n+1):
-        if i > 1:
-            d[j] = min(d[j-li[i]] + 1, d[j])
-        elif i == 1:
-            d[j] = d[j - li[i]] + 1
+        d[j] = min(d[j-li[i]] + 1, d[j])
 print(d[n])
-'''
-
-
-li = [0 for _ in range(200)]
-d = [0 for _ in range(300001)]
-for i in range(1,200):
-    li[i] = i * (i+1) * (i+2) // 6
-n = int(input())
-for i in range(1, n+1):
-    if li[i] > n:
-        break
-    for j in range(li[i], n+1):
-        if i > 1:
-            d[j] = min(d[j-li[i]] + 1, d[j])
-        elif i == 1:
-            d[j] = d[j - li[i]] + 1
-print(d[n])
+# 문제는 일부 풀리나 시간초과 발생 ㅠㅠ
